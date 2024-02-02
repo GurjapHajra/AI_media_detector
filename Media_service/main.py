c849ff4536b8b5aeb67e8cb77316caba7bcf5407
gurjap <gurjaph@my.yorku.ca>
2024-02-02 18:05:45 -0500

    backend stuff added

--- Media_service/main.py
+++ Media_service/main.py
@@ -1,6 +1,10 @@
 import functions_framework
-
 from markupsafe import escape
+from google.cloud import storage
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r'Media_service\noaivi-e4f53fa15f55.json'
+
 @functions_framework.http
 def hello_http(request):
     """HTTP Cloud Function.
@@ -13,6 +17,7 @@ def hello_http(request):
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
     request_json = request.get_json(silent=True)
+    return request_json
     request_args = request.args
 
     if request_json and "name" in request_json:
@@ -22,3 +27,31 @@ def hello_http(request):
     else:
         name = "World"
     return f"Hello {escape(name)}!"
+
+
+def upload_blob(source_file_name, destination_blob_name, bucket_name="noaivi-images"):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to upload is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    generation_match_precondition = 0
+
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
