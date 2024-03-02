ef55fdf20a867ae135837a8aa2cc1397a203f156
gurjap <gurjaph@my.yorku.ca>
2024-03-01 20:23:14 -0500

    lint fixes

--- Media_service/main.py
+++ Media_service/main.py
@@ -1,15 +1,17 @@
+'''main.py: contains the main functions for the media service'''
+
+import os
 import functions_framework
 from google.cloud.storage.blob import Blob
 from google.cloud import storage
-import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r'noaivi-e4f53fa15f55.json'
 
-main_bucket_name = "noaivi-images"
+MAIN_BUCKET_NAME = "noaivi-images"
 storage_client = storage.Client()
 
 @functions_framework
-def upload_blob(source_file_name, destination_blob_name, bucket_name=main_bucket_name):
+def upload_blob(source_file_name, destination_blob_name, bucket_name=MAIN_BUCKET_NAME):
     """Uploads a file to the bucket."""
     # The ID of your GCS bucket
     # bucket_name = "your-bucket-name"
@@ -18,7 +20,6 @@ def upload_blob(source_file_name, destination_blob_name, bucket_name=main_bucket
     # The ID of your GCS object
     # destination_blob_name = "storage-object-name"
 
-    storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
     print(":::", source_file_name)
@@ -30,15 +31,19 @@ def upload_blob(source_file_name, destination_blob_name, bucket_name=main_bucket
     # generation-match precondition using its generation number.
     generation_match_precondition = 0
 
-    blob.upload_from_file(source_file_name, if_generation_match=generation_match_precondition, content_type=source_file_name.content_type)
+    blob.upload_from_file(source_file_name,
+                          if_generation_match=generation_match_precondition,
+                          content_type=source_file_name.content_type)
 
     print(
         f"File {source_file_name} uploaded to {destination_blob_name}."
     )
 
 @functions_framework.http
-def getAssetsList(request):
-    
+def get_assets_list(request):
+    '''get_assets_list: returns a list of all assets in the bucket
+        request: flask.Request
+        name: str - optional - the name of the asset to search for'''
     args = None
     res = []
 
@@ -46,30 +51,37 @@ def getAssetsList(request):
         args = request.args or request.form
 
     if args and args.get('name'):
-        for b in storage_client.list_blobs(main_bucket_name,prefix=f"{args.get('name')}"):
+        for b in storage_client.list_blobs(MAIN_BUCKET_NAME,prefix=f"{args.get('name')}"):
             res.append(b.name)
         return res
     
-    for b in (storage_client.list_blobs(main_bucket_name)):
+    for b in (storage_client.list_blobs(MAIN_BUCKET_NAME)):
         res.append(b.name)
 
     return res
 
 @functions_framework.http
-def getAssetByName(request):
+def get_asset_by_name(request):
+    '''getAssetByName: returns the public url of the asset
+        request: flask.Request
+        name: str - required - the name of the asset to search for'''
     if request.args or request.form:
         args = request.args or request.form
     else:
         return ("no asset name provided", 404)
 
     if args and args.get('name'):
-        content = Blob.from_string(f"gs://{main_bucket_name}/{args.get('name')}")
+        content = Blob.from_string(f"gs://{MAIN_BUCKET_NAME}/{args.get('name')}")
         return content.public_url
 
     return "no asset name provided"
 
 @functions_framework.http
-def uploadAsset(request):
+def upload_asset(request):
+    '''uploadAsset: uploads an asset to the bucket
+        request: flask.Request
+        name: str - required - the name of the asset to upload
+        asset: file - required - the asset to upload'''
     print(request.files)
     if request.files:
         print(":::",request.files['asset'].content_type)
@@ -80,7 +92,6 @@ def uploadAsset(request):
 
 @functions_framework.http
 def hello_http(request):
-    
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
