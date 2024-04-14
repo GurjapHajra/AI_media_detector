961f0aa1757471c4dc4d6cf98d69a55480152fd2
GurjapHajra <61431312+GurjapHajra@users.noreply.github.com>
2024-04-14 15:57:42 -0400

    Amlify try try again (#9)
    * WWWWWW
    
    * lint sjo

--- UI/src/main.ts
+++ UI/src/main.ts
@@ -1,6 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './aiv/app.module';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
