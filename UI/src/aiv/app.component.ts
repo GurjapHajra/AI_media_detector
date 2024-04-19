d74f3165f304c15bf9bc8d64598184213212032e
gurjap <gurjaph@my.yorku.ca>
2024-04-19 19:22:50 -0400

    lint fixes

--- UI/src/aiv/app.component.ts
+++ UI/src/aiv/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,5 +9,5 @@ export class AppComponent {
   title = 'NoAiVi';
   loading = false;
 
-  constructor() { }
+  constructor() {}
 }
