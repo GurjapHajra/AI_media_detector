f0abe649f148d296d70b536bccd7b7acb423c79e
GurjapHajra <61431312+GurjapHajra@users.noreply.github.com>
2024-04-29 01:53:19 -0400

    Hashing type shi (#12)
    * Update AssetFile interface and MediaFinderComponent to include new properties and methods
    
    * mergeImages function added
    
    * Add @types/node and @types/qrcode dependencies, and update mergeImages function in MediaManagementService
    
    * Update MediaFinderComponent to include image styling
    
    * Add js-md5 dependency and update MediaFinderComponent and MediaManagementService
    
    * Add MediaServiceDBFunction to template.yaml and update MediaManagementService
    
    * Update image size in media-finder.component.scss and media-finder.component.html, add MediaServiceDBGetFunction to template.yaml, and update MediaFinderComponent and MediaManagementService
    
    * Update MediaFinderComponent and MediaManagementService

--- UI/src/aiv/services/media-management/media-management.service.ts
+++ UI/src/aiv/services/media-management/media-management.service.ts
@@ -2,19 +2,22 @@ import { AssetFile } from '@aiv/models/asset-file';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@aiv/environment/environment';
-import { Observable, map, take } from 'rxjs';
+import { Observable, map, switchMap, take } from 'rxjs';
 import {
   FlattenToGetMediaListResponse,
   GetMediaListResponse,
   PostUnsignUrlResponse,
 } from '@aiv/models/api-reponse-types';
 import { FlattenToPostUnsignUrlResponse } from '@aiv/models/api-reponse-types';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { md5 } from 'js-md5';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MediaManagementService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
   uploadMediaFiles(assets: AssetFile[]) {
     assets.forEach((asset) => {
@@ -39,22 +42,37 @@ export class MediaManagementService {
         formData.append('file', media.file);
 
         return this.http.post(res.url, formData).subscribe((val) => {
-          console.log(val);
+          this.getMediaUrl(res.fields.key).subscribe((url) => {
+            this.generateHash(url.url).subscribe((hash) => {
+              this.updateDB(media.file.name, hash);
+            });
+          });
         });
       });
   }
 
-  getMedia(): Observable<GetMediaListResponse[]> {
-    return this.http.get(`${environment.url}`).pipe(
-      map((res: any) => {
-        res = JSON.parse(res['message']);
-        res = res.reduce((acc: any, ele: any) => {
-          acc.push(FlattenToGetMediaListResponse(ele));
-          return acc;
-        }, []);
-        return res;
-      })
+  updateDB(file_name: string, file_id: string) {
+    return this.http.post(
+      `${environment.url}db?asset_id=${file_id}&asset_name=${file_name}`,
+      null
     );
+  }
+
+  getMedia(filter?: string, page?: number): Observable<GetMediaListResponse[]> {
+    return this.http
+      .get(
+        `${environment.url}/db?page=${page ?? 0}&filter_value=${filter ?? ''}`
+      )
+      .pipe(
+        map((res: any) => {
+          res = JSON.parse(res['message']);
+          res = res.reduce((acc: any, ele: any) => {
+            acc.push(FlattenToGetMediaListResponse(ele));
+            return acc;
+          }, []);
+          return res;
+        })
+      );
   }
 
   deleteMedia(id: string) {
@@ -67,7 +85,7 @@ export class MediaManagementService {
   ): Observable<PostUnsignUrlResponse> {
     return this.http
       .post(
-        `${environment.url}?file_name=${file_name}&file_type=${file_type}`,
+        `${environment.url}upload?file_name=${file_name}&file_type=${file_type}`,
         null
       )
       .pipe(map((res) => FlattenToPostUnsignUrlResponse(res)));
@@ -79,5 +97,61 @@ export class MediaManagementService {
         return { url: res['url'] };
       })
     );
+  }
+
+  getAssetFile(name: string): Observable<string> {
+    return this.http
+      .get(`${environment.url}?file_name=${name}`)
+      .pipe(switchMap((res: any) => this.fetchImageAsBase64(res.url)));
+  }
+
+  fetchImageAsBase64(url: string): Observable<string> {
+    return this.http.get(url, { responseType: 'blob' }).pipe(
+      switchMap((blob) => {
+        return new Promise<string>((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onloadend = () => resolve(reader.result as string);
+          reader.onerror = reject;
+          reader.readAsDataURL(blob);
+        });
+      })
+    );
+  }
+
+  mergeImages(image1: string, image2: string): Observable<string> {
+    const canvas = document.createElement('canvas');
+    return new Observable<string>((observer) => {
+      if (canvas !== undefined) {
+        const ctx = canvas.getContext('2d');
+        const img1 = new Image();
+        const img2 = new Image();
+
+        img1.onload = () => {
+          canvas.width = img1.width;
+          canvas.height = img1.height;
+          img2.setAttribute('crossorigin', 'anonymous');
+          img2.src = image2;
+        };
+        img2.onload = () => {
+          ctx?.drawImage(img1, 0, 0);
+          ctx?.drawImage(
+            img2,
+            img1.width - img2.width - 10,
+            10,
+            img2.width,
+            img2.height
+          );
+          observer.next(canvas.toDataURL());
+        };
+        img1.setAttribute('crossorigin', 'anonymous');
+        img1.src = image1;
+      } else {
+        return observer.next('unable to load');
+      }
+    });
+  }
+
+  generateHash(file: string): Observable<string> {
+    return this.fetchImageAsBase64(file).pipe(map((res) => md5.base64(res)));
   }
 }
