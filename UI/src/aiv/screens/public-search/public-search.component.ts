8ddc8fea9a1381b9fa0144fe3ae1653026030dbf
GursikhHajra <gursikhsinghhajra@gmail.com>
2024-05-07 10:00:25 -0400

    done

--- UI/src/aiv/screens/public-search/public-search.component.ts
+++ UI/src/aiv/screens/public-search/public-search.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { MediaManagementService } from '@aiv/services/media-management/media-management.service';
+import { addListAssets } from '@aiv/store/remote-assets-store/remote-asset-store.actions';
+import { getListAssets } from '@aiv/store/remote-assets-store/remote-asset-store.selectors';
+import { Observable, catchError, map, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-public-search',
@@ -6,5 +11,93 @@ import { Component } from '@angular/core';
   styleUrl: './public-search.component.scss'
 })
 export class PublicSearchComponent {
+  protected picUrl: string = '';
+  protected filter: string = '';
 
+  protected searchResult = this.store.select(getListAssets).pipe(
+    map((assets) => {
+      return assets.map((item) => {
+        let date = new Date(item.last_modified).toLocaleDateString();
+
+        return {
+          name: item.asset_name,
+        };
+      });
+    })
+  );
+
+  displayedColumns: string[] = [
+    'name',
+    'view',
+  ];
+
+  constructor(
+    private store: Store,
+    private MediaManagementService: MediaManagementService
+  ) { }
+
+  protected searched() {
+    this.MediaManagementService.getAssetListFromDb(this.filter).subscribe(
+      (assets) => {
+        this.store.dispatch(addListAssets({ ListAssets: assets }));
+      }
+    );
+  }
+
+  protected openImage(key: string) {
+    this.MediaManagementService.getAssetPreSignUrl(key).subscribe((res) => {
+      take(1);
+      this.picUrl = res.url;
+    });
+  }
+
+  protected mergeImages(url: string, name: string) {
+    // QRCode.toDataURL('flaticon.com/123456789012=jlj;', {
+    //   margin: 1,
+    //   color: {
+    //     dark: '#6A1B9A',
+    //     light: '#69F0AE',
+    //   },
+    // }).then((qrUrl) => {
+    //   this.MediaManagementService.mergeImages(url, this.localImage()).subscribe(
+    //     (res) => {
+    //       this.picUrl = res;
+    //     }
+    //   );
+    // });
+    this.getAssetId(name).subscribe((id) => {
+      this.joinLogoWithString(id).subscribe((res) => {
+        this.MediaManagementService.mergeImages(url, res).subscribe((res) => {
+          this.picUrl = res;
+        });
+      });
+    });
+  }
+
+  protected getAssetId(name: string): Observable<string> {
+    return this.store.select(getListAssets).pipe(
+      map((res) => {
+        return res.find((item) => item.asset_name === name)?.asset_id ?? '';
+      })
+    );
+  }
+
+  protected joinLogoWithString(hash: string): Observable<string> {
+    let img = new Image();
+    img.src = String.raw`../../../assets/codeLogo.png`;
+
+    return new Observable<string>((observer) => {
+      img.onload = () => {
+        let c = document.createElement('canvas');
+        c.width = img.width;
+        c.height = img.height;
+        let ctx = c.getContext('2d') ?? new CanvasRenderingContext2D();
+        ctx.font = '50px Arial';
+        ctx.drawImage(img, 0, 0);
+        ctx.fillText(hash, 85, c.height - 10);
+        observer.next(c.toDataURL());
+        observer.complete();
+      };
+    });
+  }
 }
--- UI/src/aiv/screens/public-search/public-search.module.ts
+++ UI/src/aiv/screens/public-search/public-search.module.ts
@@ -3,7 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { PublicSearchRoutingModule } from './public-search-routing.module';
 import { PublicSearchComponent } from './public-search.component';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { StoreModule } from '@ngrx/store';
+import { MatInputModule } from '@angular/material/input';
+import { remoteAssetFeature } from '@aiv/store/remote-assets-store/remote-asset-store.reducer';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MediaManagementService } from '@aiv/services/media-management/media-management.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +18,14 @@ import { PublicSearchComponent } from './public-search.component';
   ],
   imports: [
     CommonModule,
-    PublicSearchRoutingModule
-  ]
+    PublicSearchRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule,
+    StoreModule.forFeature(remoteAssetFeature.name, remoteAssetFeature.reducer),
+  ],
+  providers: [MediaManagementService],
 })
 export class PublicSearchModule { }
