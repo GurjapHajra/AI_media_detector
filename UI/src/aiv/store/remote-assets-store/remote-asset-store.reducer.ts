de953411a549f0f1d53838eb6a7eb5d6e6f031f3
GurjapHajra <61431312+GurjapHajra@users.noreply.github.com>
2024-05-14 20:55:43 -0400

    feat: Refactor remote-asset-store-effects to include deletingAsset selector (#37)
    Refactor the remote-asset-store-effects module to include the deletingAsset selector. This change adds the getDeletingAsset selector to the module's imports and uses it in the deleteAsset$ effect. The effect now sets the deletingAsset property in the state to the asset name being deleted and removes the deleted asset from the Listfiles array. This improvement enhances the accuracy and traceability of asset deletion.

--- UI/src/aiv/store/remote-assets-store/remote-asset-store.reducer.ts
+++ UI/src/aiv/store/remote-assets-store/remote-asset-store.reducer.ts
@@ -1,5 +1,10 @@
 import { Action, createFeature, createReducer, on } from '@ngrx/store';
-import { addListAssets, reset } from './remote-asset-store.actions';
+import {
+  addListAssets,
+  deleteAsset,
+  deleteAssetSuccess,
+  reset,
+} from './remote-asset-store.actions';
 import { remoteAssetStoreState, initialState } from './state';
 import { ASSET_STATE_NAME } from './remote-asset-store.selectors';
 
@@ -8,8 +13,21 @@ const _reducer = createReducer(
   on(addListAssets, (state, { ListAssets }): remoteAssetStoreState => {
     return { ...state, Listfiles: ListAssets };
   }),
+
   on(reset, (state): remoteAssetStoreState => {
     return { ...state, Listfiles: [] };
+  }),
+
+  on(deleteAsset, (state, { assetName }): remoteAssetStoreState => {
+    return { ...state, deletingAsset: assetName };
+  }),
+
+  on(deleteAssetSuccess, (state): remoteAssetStoreState => {
+    let newList = state.Listfiles.filter(
+      (item) => item.asset_name !== state.deletingAsset
+    );
+
+    return { ...state, deletingAsset: undefined, Listfiles: newList };
   })
 );
 
--- UI/src/aiv/store/remote-assets-store/state.ts
+++ UI/src/aiv/store/remote-assets-store/state.ts
@@ -3,9 +3,11 @@ import { GetMediaListResponse } from '@aiv/models/api-reponse-types';
 export interface remoteAssetStoreState {
   Listfiles: GetMediaListResponse[];
   length: number;
+  deletingAsset: string | undefined;
 }
 
 export const initialState: remoteAssetStoreState = {
   Listfiles: [],
   length: 0,
+  deletingAsset: undefined,
 };
